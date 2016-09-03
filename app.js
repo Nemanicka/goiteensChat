dc21c85c87c87bfedbcb71b6655ec7573b7f0ef5
Oleksii Tretiak <ext-oleksiy.tretyak@here.com>
2016-09-03 18:23:04 +0300

    added CORS

--- app.js
+++ app.js
@@ -26,6 +26,26 @@ app.use(express.static(path.join(__dirname, 'public','javascripts/index.js')));
 app.use('/', routes);
 app.use('/users', users);
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+        res.setHeader('Access-Control-Allow-Origin', 'http://95.67.110.244:7474');
+
+            // Request methods you wish to allow
+                res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+                    // Request headers you wish to allow
+                        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+                            // Set to true if you need the website to include cookies in the requests sent
+                                // to the API (e.g. in case you use sessions)
+                                    res.setHeader('Access-Control-Allow-Credentials', true);
+
+                                        // Pass to next layer of middleware
+                                            next();
+                                            });
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
